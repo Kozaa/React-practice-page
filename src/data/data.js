aad3c1145f89d90851f296d0974ac06a2eca9e10
Kacper Kozanecki <kacper2424@interia.eu>
2020-08-27 18:48:25 +0200

    fixed planet distances

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const planetData = [
       name: 'Mercury',
       description: ['4.880 km','0.39 AU (58.000.000 km from the Sun)', 'Mercury’s craters are named after famous artists, musicians and authors.'],
       width: 5,
-      distance: 5850,
+      distance: 58000,
       color: '#bbb'
     },
     {
@@ -12,7 +12,7 @@ export const planetData = [
       name: 'Venus',
       description: ['12.100 km', '0.72 AU (108.000.000 km from the Sun)', 'Venus is the hottest planet in the solar system.'],
       width: 12,
-      distance: 4950,
+      distance: 50000,
       color: '#d4b655'
     },
     {
@@ -20,7 +20,7 @@ export const planetData = [
       name: 'Earth',
       description: ['12.800 km','1 AU (150.000.000 km from the Sun)', 'Earth’s atmosphere protects us from meteoroids and radiation from the Sun.'],
       width: 13,
-      distance: 100500,
+      distance: 42000,
       color: '#3b7cd9'
     },
     {
@@ -28,7 +28,7 @@ export const planetData = [
       name: 'Mars',
       description: ['6.800 km', '1.5 AU (228.000.000 km from the Sun)', 'There have been more missions to Mars than any other planet.'],
       width: 7,
-      distance: 75000,
+      distance: 78000,
       color: '#fc4a03'
     },
     {
@@ -36,7 +36,7 @@ export const planetData = [
       name: 'Jupiter',
       description: ['142.000 km', '5.2 AU (778.000.000 km from the Sun)', 'Jupiter has more than double the mass of all the other planets combined.'],
       width: 140,
-      distance: 705000,
+      distance: 555000,
       color: '#e6bb81'
     },
     {
@@ -44,7 +44,7 @@ export const planetData = [
       name: 'Saturn',
       description: ['120.000 km', '9.5 AU (1.430.000.000 km from the Sun)','Saturn has more moons than any other planet in the Solar System.'],
       width: 120,
-      distance: 720000,
+      distance: 652000,
       color: '#ffea8c'
     },
     {
@@ -52,7 +52,7 @@ export const planetData = [
       name: 'Uranus',
       description: ['51.800 km', '19.2 AU (2.870.000.000 km from the Sun)', 'Uranus has only been visited by a single spacecraft, Voyager 2.'],
       width: 51,
-      distance: 2160000,
+      distance: 1440000,
       color: '#7cf7f1'
     },
     {
@@ -60,7 +60,7 @@ export const planetData = [
       name: 'Neptune',
       description: ['49.500 km', '30.1 AU (4.500.000.000 km from the Sun)', 'It takes more than 4 hours for light to reach Neptune from the Sun.'],
       width: 50,
-      distance: 2355000,
+      distance: 1630000,
       color: '#2f00ff'
     }
   ];
